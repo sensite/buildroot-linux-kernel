b7e7fe586d34313cf87173c603e54ef6896a6172
Yang Huang <yang@mythserver.(none)>
2013-02-08 20:38:35 -0500

    setting 1080p24, forgot about video clk

--- drivers/amlogic/display/vout/tvoutc.c
+++ drivers/amlogic/display/vout/tvoutc.c
@@ -221,6 +221,7 @@ int tvoutc_setclk(tvmode_t mode)
 		case TVMODE_1080I_50HZ:
 		case TVMODE_1080P:
 		case TVMODE_1080P_50HZ:
+		case TVMODE_1080P_24HZ:
 			  setreg(&hd[xtal]);
 			  if(xtal == 1)
 			  {
